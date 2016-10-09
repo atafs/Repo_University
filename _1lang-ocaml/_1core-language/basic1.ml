0df27a2bf4fede3752345a1941f85b1e964bd1bb
americo <atafs@iscte-iul.pt>
2016-10-09 03:11:03 +0100

    US: .gitignore

--- _1lang-ocaml/_1core-language/basic1.ml
+++ _1lang-ocaml/_1core-language/basic1.ml
@@ -1,3 +1,5 @@
-let x = 1 (* this is a comment *) + 4;;
-print_int x;;
+let x = 1 (* this is a comment *) + 2;;
+let y = 1 (* this is a comment *) + 20;;
+let z = x + y;;
+print_int z;;
 print_string "\n";;
