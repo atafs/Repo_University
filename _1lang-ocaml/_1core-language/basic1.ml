1e3c80e1646680bf9b83b044ee49e704673263a0
atafs <atafs@iscte-iul.pt>
2016-10-11 00:08:34 +0100

    PROG: delete old names

--- _1lang-ocaml/_1core-language/basic1.ml
+++
@@ -1,5 +0,0 @@
-let x = 1 (* this is a comment *) + 2;;
-let y = 1 (* this is a comment *) + 20;;
-let z = x + y;;
-print_int z;;
-print_string "\n";;
--- _1lang-ocaml/_1core-language/prog_001.ml
+++
@@ -1,8 +0,0 @@
-(* rec: recursive*)
-(* gcd: greatest common divisor of two positive numbers a and b. We assume a>b*)
-let rec gcd a b =
-  let r = a mod b in
-  if r = 0 then
-    b
-  else
-    gcd b r
